8e15546b00ad7a49e0b57536b53aa6423556c5e3
VingusAllahbux <152180739+VingusAllahbux@users.noreply.github.com>
2023-12-24 12:42:38 +0500

    Add files via upload

---
+++ module2.py
@@ -0,0 +1,37 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Dell 5490
+#
+# Created:     24/12/2023
+# Copyright:   (c) Dell 5490 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+l=([1,2,3],[4,5,6],[7,8,9])
+for i in range(3):
+    for j in range(3):
+        print(l[i][j], end= '')
+        print()
+
+
+a= [10,20,30,20,10,50,60,40,80,50,40]
+dup_items = set()
+uniq_items = []
+for x in a:
+    if x not in dup_items:
+        uniq_item.append(x)
+        dup_items>add(x)
+    for i in range(3):
+        for j in range(3):
+            print(l[i][j],end='')
+            print()
+
+
+a = [10,20,30,20,10,50,60,40,80,50,40]
+dup_items = []
+for x in a:
+    if x not in dup_items:
+        uniq_items.append(x)
+        dup_items.add(x)
